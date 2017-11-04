c6ae48ee81568cc215327bc171e0ed9cc1949e3a
Ahmed ElSayed <ahmels@microsoft.com>
2017-11-04 10:08:38 -0700

    remove _ from provider name for azure

--- lib/omnigollum.rb
+++ lib/omnigollum.rb
@@ -88,7 +88,7 @@ module Omnigollum
           origin = '/'
         end
 
-        redirect (request.script_name || '') + options[:route_prefix] + '/auth/' + options[:provider_names].first.to_s + "?origin=" +
+        redirect (request.script_name || '') + options[:route_prefix] + '/auth/' + options[:provider_names].first.to_s.gsub('_', '') + "?origin=" +
            CGI.escape(origin)
       else
          auth_config
